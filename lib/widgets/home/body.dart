49eaa96d5fbe686803e8bfdb5bdf7bc548bead98
oluwakeye-john <bjohnoluwakeye@gmail.com>
2021-05-18 03:02:09 +0100

    bottom nav

--- lib/widgets/home/body.dart
+++ lib/widgets/home/body.dart
@@ -1,4 +1,6 @@
+import 'package:fintech/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeBody extends StatelessWidget {
   final Widget child;
@@ -10,6 +12,36 @@ class HomeBody extends StatelessWidget {
 
     double horPadding = size.width * 0.06;
     return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        currentIndex: 1,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Color(0xFF200E32),
+        items: [
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/images/icons/home.svg',
+                color: primaryColor,
+              ),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/images/icons/card.svg',
+              ),
+              label: "Plans"),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/images/icons/graph.svg'),
+              label: "History"),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/images/icons/category.svg'),
+              label: "Settings"),
+        ],
+      ),
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.only(left: horPadding, right: horPadding),
